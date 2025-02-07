9faa255cafc1dc6d5730aa90c253799deea587c3
MohammedAli200 <alimohammed3731@gmail.com>
2025-02-07 09:04:49 +0530

    Add files via upload

---
+++ day-4/script1.js
@@ -0,0 +1,6 @@
+document.write("this is my first javascript program <br><br>");
+document.write("this is my second javascript program <br> <br>");
+// window.alert("hi");
+console.log("this is my first javascript program ");
+ var promt=prompt("enter your name "); 
+document.write(promt);
